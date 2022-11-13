2c6dd053901c75feaa110e682aef543336b965e7
Sebastian Jensen <apps@tsjdev-apps.de>
2022-11-13 14:36:04 +0100

    Add sample .NET MAUI app.

---
+++ samples/ICanHazDadJokeSampleApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ICanHazDadJokeSampleApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
