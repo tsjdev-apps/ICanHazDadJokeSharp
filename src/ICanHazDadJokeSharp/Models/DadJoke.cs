19493219446d0af08acdad25efe2ff52052c8b77
Sebastian Jensen <apps@tsjdev-apps.de>
2022-11-13 11:50:57 +0100

    Add model classes.

---
+++ src/ICanHazDadJokeSharp/Models/DadJoke.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ICanHazDadJokeSharp
+{
+    /// <summary>
+    ///     Representation of a dad joke.
+    /// </summary>
+    public class DadJoke
+    {
+        /// <summary>
+        ///     Gets or Sets the ID of the joke.
+        /// </summary>
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        /// <summary>
+        ///     Gets or Sets the joke content.
+        /// </summary>
+        [JsonPropertyName("joke")]
+        public string Joke { get; set; }
+
+        /// <summary>
+        ///     Gets or Sets the status of the reponse.
+        /// </summary>
+        [JsonPropertyName("status")]
+        public int Status { get; set; }
+    }
+}
